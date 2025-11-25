dc732afae99ca79c1ff0fc77e8d2466916cf0459
Santiago <santizurlo05@gmail.com>
2025-11-24 22:04:39 -0300

    Metodos Get

--- index.js
+++ index.js
@@ -1,15 +1,77 @@
 import express from 'express';
 const app = express();
 
-import  method from './src/middlewares/method.js';
-app.use(method);
+const products = [
+    { 
+    id: 1, 
+    name: 'Producto 1', 
+    price: 100, 
+    categories: ['categoria1', 'categoria2'],
+    },
+    { 
+    id: 2, 
+    name: 'Producto 2', 
+    price: 200, 
+    categories: ['categoria2', 'categoria3'],
+    },
+    { 
+    id: 3, 
+    name: 'Producto 3', 
+    price: 300, 
+    categories: ['categoria4', 'categoria6'],
+},
+];
 
-app.get('/', (req, res) => {
-    res.json({ message: "Bievenidos a mi API REST" });
+import welcome from './src/middlewares/welcome.js';
+app.get('/', welcome);
+
+app.get('/products', (req, res) => {
+    const {category} = req.query;   
+
+        if(category) {
+            const productsFiltered = products.filter(item => 
+             item.categories.includes(category)
+         );
+        res.json(productsFiltered);
+        return;
+    } 
+    res.json(products);
+});
+
+app.get('/products/search', (req, res) => {
+    const {name} = req.query; 
+
+    if(!name) {
+       return res.status(400).json({ error: 'Falta el parámetro name' });
+    }
+
+    const productsFiltered = products.filter(item => 
+        item.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    if(productsFiltered.length == 0) {
+       return res.status(404).json({ error: 'No se encontraron productos' });
+    }
+
+    res.json(productsFiltered);
+});      
+
+
+app.get('/products/:id', (req, res) => {
+    const  id  = parseInt(req.params.id);
+
+    const product = products.find((item) => item.id == id);
+    if (!product) {
+        res.status(404).json({ error: 'No existe el producto' });
+    }
+
+    res.json(product);
 });
 
 import notFound from './src/middlewares/not-found.js';
 app.use(notFound);
+
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
--- src/middlewares/method.js
+++
@@ -1,5 +0,0 @@
-export default (req, res, next) => {
-    //res.json({ message: "Hola API en mantenimiento" });
-    console.log(req.method);
-    next();
-}

6e936cb8e6fca8274e5ba95fe80d92f46378d00d
Santiago <santizurlo05@gmail.com>
2025-11-25 20:40:21 -0300

    Autenticación agregada

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 import welcome from './src/middlewares/welcome.js';
 app.get('/', welcome);
 
+import authRouter from './src/routes/auth.router.js';  
+app.use('/api/auth', authRouter);
+
 import productsRouter from './src/routes/products.router.js';
 app.use('/api', productsRouter);
 
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt";
+import { createUser, findUserByEmail }  from "../models/User.js";
+export const register = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).json({ message: "Email y contraseña requeridos." });
+    }
+
+    const existingUser = await findUserByEmail(email);
+
+    if(existingUser) {
+        return res.status(409).json({ message: "El usuario ya existe." });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = createUser(email, passwordHash);
+
+    if(!user) {
+        return res.sendStatus(503);
+    }
+
+    res.status(201).json({ id: user.id, email: user.email });
+};

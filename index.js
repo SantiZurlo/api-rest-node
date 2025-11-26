0ac134ce6d533e288eeea1311996c5034c091ee3
Santiago <santizurlo05@gmail.com>
2025-11-25 21:11:41 -0300

    Login y JWT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import "dotenv/config";
+import { verifyToken } from "./src/middlewares/verify-token.js";
 const app = express();
 
 app.use(express.json());
@@ -11,7 +12,7 @@ import authRouter from './src/routes/auth.router.js';
 app.use('/api/auth', authRouter);
 
 import productsRouter from './src/routes/products.router.js';
-app.use('/api', productsRouter);
+app.use('/api', verifyToken,productsRouter);
 
 import notFound from './src/middlewares/not-found.js';
 app.use(notFound);
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { createUser, findUserByEmail }  from "../models/User.js";
 export const register = async (req, res) => {
     const { email, password } = req.body;
@@ -15,11 +16,38 @@ export const register = async (req, res) => {
 
     const passwordHash = await bcrypt.hash(password, 10);
 
-    const user = createUser(email, passwordHash);
+    const user = await createUser(email, passwordHash);
 
     if(!user) {
         return res.sendStatus(503);
     }
 
     res.status(201).json({ id: user.id, email: user.email });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).json({ message: "Email y contraseña requeridos." });
+    }
+
+    const user = await findUserByEmail(email);
+    if (!user) {
+        return res.status(401).json({ message: "Credenciales inválidas." });
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+        return res.status(401).json({ message: "Credenciales inválidas." });
+    }
+
+    const token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        {
+         expiresIn: "1h" 
+        }
+    );
+    res.json({ token });
 };
